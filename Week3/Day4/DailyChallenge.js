dab42ca6aaa81ac9dbcbd721c27771d6ef46a657
Ethan <EthanA120@Google.com>
2022-01-12 00:16:11 +0200

    Final

--- Week3/Day4/DailyChallenge.js
+++ Week3/Day4/DailyChallenge.js
@@ -1,3 +1,18 @@
+/* TASK: Not Bad
+
+    1. Create a variable called sentence. The value of the variable should be a string that
+        contains the words "not" and "bad". For example, "The movie is not that bad, I like it".
+    2. Create a variable called wordNot where it's value is the first appearance of the substring9
+        "not" (from the sentence variable).
+    3. Create a variable called wordBad where it's value is the first appearance of the substring
+        "bad" (from the sentence variable).
+    4. If the word "bad" comes after the word "not", you should replace the whole "not...bad"
+        substring with "good", then console.log the result.
+        For example, the result here should be: "The movie is good, I like it"
+    5. If the word "bad" does not come after "not" or the words are not in the sentence,
+        console.log the original sentence.
+*/
+
 let sentence = "The movie is not that bad, I like it";
 let wordNot = sentence.indexOf("not");
 let wordBad = sentence.indexOf("bad");
--- Week3/Day4/XP.js
+++ Week3/Day4/XP.js
@@ -1,5 +1,15 @@
-// Exercise 1: Simple if/else statement
-console.log("Ex1:")
+/* TASK: Simple if/else statement
+
+    1. Create 2 variables, x and y. Each of them should have a different numeric value.
+    2. Write an if/else statement that checks which number is bigger.
+
+    Example:
+        let x = 5;
+        let y = 2;
+
+        You should display:
+        x is the biggest number
+*/
 let x = 5;
 let y = 2;
 
@@ -9,8 +19,16 @@ if (x > y) {
     console.log(`${y} is the biggest number`);
 }
 
-// Exercise 2: Chihuahua
-console.log("Ex2:")
+/* TASK: Chihuahua
+    
+    1. Create a variable called newDog where it's value is "Chihuahua".
+    2. Check and display how many letters are in newDog.
+    3. Display the newDog variable in uppercase and then in lowercase
+        (no need to create new variables, just console.log twice).
+    4. Check if the variable newDog is equal to "Chihuahua"
+        - if it does, display '1 love Chihuahuas, it's my favorite dog breed'
+        - else, console. log '1 dont care, I prefer cats'
+*/
 let newDog = "Chihuahua";
 console.log(newDog.length);
 console.log(newDog.toUpperCase(), newDog.toLowerCase());
@@ -21,8 +39,13 @@ if (newDog == "Chihuahua") {
     console.log("I dont care, I prefer cats");
 }
 
-// Exercise 3: Even or Odd
-console.log("Ex3:")
+/* TASK: Even or Odd
+        
+    1. Prompt the user for a number and save it to a variable.
+    2. Check whether the variable is even or odd.
+        - If it is even, display: "x is an even number". Where x is the actual number the user chose.
+        - If it is odd, display: "x is an odd number". Where x is the actual number the user chose.
+*/
 let userInput = parseInt(prompt("Enter a number"));
 
 if (userInput % 2 == 0) {
@@ -31,8 +54,20 @@ if (userInput % 2 == 0) {
     console.log(`${userInput} is an odd number`);
 }
 
-// Exercise 4: Group Chat
-console.log("Ex4:")
+/* TASK: Group Chat
+    
+    Below is an array of users that are online in a group chat.
+        let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"]
+
+    Using the array above, console.log the number of users that are connected to the group chat based on the following rules:
+        - If there is no users (the users array is empty), console.log “no one is online”.
+        - If there is 1 user, console.log “<name_user> is online”.
+        - If there are 2 users, console.log “<name_user1> and <name_user2> are online”.
+        - If there are more than 2 users, console.log the first two names in the users array and the number of additional users online.
+        
+        For example, if there are 5 users, it should display:
+            name_user1, name_user2 and 3 more are online
+*/
 let users = ["Lea123", "Princess45", "cat&doglovers", "helooo@000"];
 
 if (users.length == 0) {
