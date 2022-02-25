b7191ba1ae4de8e026ebdf3628a646a284f55f06
Ethan <EthanA120@Google.com>
2022-02-25 21:34:24 +0200

    Final

--- Week9/Day4/DailyChallenge.py
+++ Week9/Day4/DailyChallenge.py
@@ -37,31 +37,87 @@ class Node:
             Implement a method search(value) which return the node containing this value inside the tree.
     """
 
-    def __init__(self, value):
-        self.value = value
+    # Create a node
+    def __init__(self, key):
+        self.key = key
         self.left = None
         self.right = None
 
-    def get_left(self):
-        return self.left
 
-    def get_right(self):
-        return self.right
+# Inorder traversal
+def inorder(root):
+    if root is not None:
+        # Traverse left
+        inorder(root.left)
+        # Traverse root
+        print(str(root.key) + "->", end=' ')
+        # Traverse right
+        inorder(root.right)
 
-    def set_left(self):
-        self.left = left
 
-    def set_right(self):
-        self.right = right
+# Insert a node
+def insert(node, key):
+    # Return a new node if the tree is empty
+    if node is None:
+        return Node(key)
+    # Traverse to the right place and insert the node
+    if key < node.key:
+        node.left = insert(node.left, key)
+    else:
+        node.right = insert(node.right, key)
+    return node
 
-    def set_value(self):
-        self.value = value
 
-    def get_value(self):
-        return self.value
+# Find the inorder successor
+def min_value_node(node):
+    current = node
+    # Find the leftmost leaf
+    while current.left is not None:
+        current = current.left
+    return current
 
-def add_node(node):
-    pass
 
-def search(value):
-    pass
+# Deleting a node
+def delete_node(root, key):
+    # Return if the tree is empty
+    if root is None:
+        return root
+    # Find the node to be deleted
+    if key < root.key:
+        root.left = delete_node(root.left, key)
+    elif key > root.key:
+        root.right = delete_node(root.right, key)
+    else:
+        # If the node is with only one child or no child
+        if root.left is None:
+            temp = root.right
+            root = None
+            return temp
+        elif root.right is None:
+            temp = root.left
+            root = None
+            return temp
+        # If the node has two children,
+        # place the inorder successor in position of the node to be deleted
+        temp = min_value_node(root.right)
+        root.key = temp.key
+        # Delete the inorder successor
+        root.right = delete_node(root.right, temp.key)
+    return root
+
+
+root = None
+root = insert(root, 8)
+root = insert(root, 3)
+root = insert(root, 1)
+root = insert(root, 6)
+root = insert(root, 7)
+root = insert(root, 10)
+root = insert(root, 14)
+root = insert(root, 4)
+print("Inorder traversal: ", end=' ')
+inorder(root)
+print("\nDelete 10")
+root = delete_node(root, 10)
+print("Inorder traversal: ", end=' ')
+inorder(root)
