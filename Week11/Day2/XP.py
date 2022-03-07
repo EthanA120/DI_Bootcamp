e796510fe985f7515d569d0fea4ee21facfa5c10
Ethan <EthanA120@Google.com>
2022-03-08 00:38:45 +0200

    Final

---
+++ Week11/Day2/XP.py
@@ -0,0 +1,29 @@
+# TASK: Exercise 1
+def ex1():
+    """
+        Instructions
+            1. Use pip to install the flask module.
+            2. Run the command import flask in a python shell.
+            3. Run the command dir(flask) to see the different methods that come along with flask.
+    """
+    pass
+
+
+# TASK: Exercise 2
+def ex2():
+    """
+        Using Flask, create a template for your CV.
+        Your CV should contain:
+
+            - Your name
+            - A picture
+            - Your hobbies
+            - Your skills
+            - Your strengths
+            - Your weaknesses
+
+            Bonus: Add some CSS
+            Hint : To add some CSS take a look at the video called Static Files in the Online Learning section.
+
+    """
+    pass
